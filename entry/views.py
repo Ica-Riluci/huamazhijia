02054e2d4e2bb839f06e1233cd8fa26e8f1a8a90
Ica Riluci <weiyh15@mails.tsinghua.edu.cn>
2017-08-14 00:34:02 +0800

    [update]<validation>{student login validation}

--- entry/views.py
+++ entry/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 
 def passin(request):
-    context = {}
+    context = {
+        'warninfo' : '',
+    }
+    if (settings.USER_NOT_EXIST):
+        context['warninfo'] = '[登录失败]用户不存在'
+        settings.USER_NOT_EXIST = False
+    if (settings.PW_NOT_CORRECT):
+        context['warninfo'] = '[登录失败]密码不正确'
+        settings.PW_NOT_CORRECT = False
+    if (settings.USER_NULL):
+        context['warninfo'] = '[登录失败]用户名为空'
+        settings.USER_NULL = False
+    if (settings.PW_NULL):
+        context['warninfo'] = '[登录失败]密码为空'
+        settings.PW_NULL = False
     return render(request, 'entry/index.html', context)
 
 def studentsignup(request):
--- validation/views.py
+++ validation/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.conf import settings
+from django.urls import reverse
+
+from .models import Student
 
 def studentlogin(request):
-    return HttpResponse('studentlogin')
+    if (request.method == 'POST'):
+        _username = request.POST['username']
+        _password = request.POST['password']
+        if (_username == ''):
+            settings.USER_NULL = True
+            return HttpResponseRedirect(reverse('entry:passin', args=()))
+        if (_password == ''):
+            settings.PW_NULL = True
+            return HttpResponseRedirect(reverse('entry:passin', args=()))
+        try:
+            user = Student.objects.get(username=_username)
+            return HttpResponse('studentlogin')
+        except:
+            settings.USER_NOT_EXIST = True
+            return HttpResponseRedirect(reverse('entry:passin', args=()))
